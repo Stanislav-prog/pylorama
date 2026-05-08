8c481b6a8a8904af6672f44a01c06142f4814c4c
Stanislav-prog <ultrariskdetected@gmail.com>
2026-05-08 22:13:17 +0300

    Added product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+   id = models.IntegerField(primary_key=True, unique=True)
+   name = models.CharField(max_length=100)
+   material = models.CharField(max_length=100)
+   price = models.DecimalField(decimal_places=2, max_digits=10)
+   quantity = models.IntegerField()
+
+   def __str__(self):
+      return f'{self.name}'

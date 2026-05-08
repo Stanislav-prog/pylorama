41074b1cf1384c7cf3535e4b2b2e298a6e3b1380
Stanislav-prog <ultrariskdetected@gmail.com>
2026-05-08 23:32:06 +0300

    Implemented products page

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import products
+from .views import products, add_success, add_failure
 
 urlpatterns = [
-   path("products/", products, name="products")
+   path("products/", products, name="products"),
+   path("products/add-success", add_success, name="add-success"),
+   path("products/add-failure", add_failure, name="add-failure"),
 ]
--- products/views.py
+++ products/views.py
@@ -4,25 +4,39 @@ from .models import Product
 # Create your views here.
 
 def index(request):
-   if request.method == "GET":
-      form = ProductAddingForm()
-   elif request.method == "POST":
-      form = ProductAddingForm(request.POST)
-      if form.is_valid():
-         id = form.cleaned_data["id"]
-         name = form.cleaned_data["name"]
-         material = form.cleaned_data["material"]
-         price = form.cleaned_data["price"]
-         quantity = form.cleaned_data["quantity"]
-         _, was_created = Product.objects.get_or_create(id=id, name=name, material=material, price=price, quantity=quantity)
-         if was_created:
+   try:
+      if request.method == "GET":
+         form = ProductAddingForm()
+      elif request.method == "POST":
+         form = ProductAddingForm(request.POST)
+
+         if form.is_valid():
+            id = form.cleaned_data["id"]
+            name = form.cleaned_data["name"]
+            material = form.cleaned_data["material"]
+            price = form.cleaned_data["price"]
+            quantity = form.cleaned_data["quantity"]
+            Product.objects.get_or_create(id=id, name=name, material=material, price=price, quantity=quantity)
             return redirect("add-success")
-         else:
-            return redirect("add-failure")
+         
+   except Exception as exc:
+      return redirect("add-failure")
 
    return render(request, "products/index.html", {
       "form": form
    })
 
 def products(request):
-   return render(request, "products/products/products.html")
+   products = Product.objects.all()
+   field_names = [f.name for f in Product._meta.get_fields()]
+
+   return render(request, "products/products/products.html", {
+      "table_headers": field_names,
+      "products": products
+   })
+
+def add_success(request):
+   return render(request, "products/add-results/add-success.html")
+
+def add_failure(request):
+   return render(request, "products/add-results/add-failure.html")

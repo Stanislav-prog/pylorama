db0a342686eb016cc86df35783581e88beabe408
Stanislav-prog <ultrariskdetected@gmail.com>
2026-05-08 21:42:37 +0300

    Added two templates and configured basic urls

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-   return HttpResponse("This is the first view of the pylorama")
+   return render(request, "products/index.html")
 
 def products(request):
-   return HttpResponse("This is a products page. There should be a form to add products to a table")
+   return render(request, "products/products/products.html")
